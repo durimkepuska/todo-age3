ffce83f5e70485445fc965fb64bd3ef3ff8ec6a2
durimkepuska <durimkepuska@gmail.com>
2022-04-14 20:08:12 +0200

    rritja e rrogave

--- Sessions/JS arrays/functions.js
+++ Sessions/JS arrays/functions.js
@@ -1,8 +1,16 @@
-let ages = [10, 20, 30, 40, 50];
+// let ages = [10, 20, 30, 40, 50];
+//
+// let temp = ages[1];
+// ages[1] = ages[2];
+// ages[2] = temp;
+//
+// console.log(ages)
 
-let temp = ages[1];
-ages[1] = ages[2];
-ages[2] = temp;
+let salaries = [100, 200, 300, 400, 500, 1000, 2000, 3000];
+for (let i = 0; i < salaries.length; i++) {
+    if (salaries[i] < 1000) {
+        salaries[i] = salaries[i] + 100;
+    }
+}
 
-console.log(ages)
-
+console.log(salaries)
