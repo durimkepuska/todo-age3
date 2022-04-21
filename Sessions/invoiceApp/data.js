9d77c0a3b8c528c7e61ab4406441855a5d1c6b32
durimkepuska <durimkepuska@gmail.com>
2022-04-21 20:08:45 +0200

    lift v3

---
+++ Sessions/invoiceApp/data.js
@@ -0,0 +1,34 @@
+const invoices = [
+    {
+        invoiceId: 1,
+        clientName: "Fahria SHPK",
+        clientNUI: "178 444 5500",
+        clientAddress: "MIthat Frasheri",
+        clientPhone: "048 800 143",
+        productsOrServices: [
+            {
+                name: "Speca",
+                amount: 5,
+                unit: "kg",
+                pricePerUnit: 8,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    }
+]
