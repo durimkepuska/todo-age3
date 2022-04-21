d8efda33db1a0dc6c8b2dfef0ef163b25cb47315
durimkepuska <durimkepuska@gmail.com>
2022-04-21 20:45:55 +0200

    invoice app

--- Sessions/invoiceApp/data.js
+++ Sessions/invoiceApp/data.js
@@ -30,5 +30,293 @@ const invoices = [
                 currency: "euro"
             }
         ]
+    },
+    {
+        invoiceId: 3,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 4,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 5,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 6,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 7,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 8,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 9,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 10,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
+    },
+    {
+        invoiceId: 2,
+        clientName: "Curri SHPK",
+        clientNUI: "811 444 555",
+        clientAddress: "Sami Frasheri",
+        clientPhone: "048 8550 143",
+        productsOrServices: [
+            {
+                name: "Tranguj",
+                amount: 2,
+                unit: "kg",
+                pricePerUnit: 4,
+                currency: "euro"
+            }
+        ]
     }
 ]
--- Sessions/invoiceApp/renderer.js
+++ Sessions/invoiceApp/renderer.js
@@ -2,13 +2,18 @@ function renderInvoices() {
     for (let i = 0; i < invoices.length; i++) {
         document.getElementById("invoices").innerHTML =
             document.getElementById("invoices").innerHTML +
-            `<div class="invoice">
-                <div>${invoices[i].invoiceId}</div>
-                <div>${invoices[i].clientName}</div>
-                <div>${invoices[i].clientNUI}</div>
-                <div>${invoices[i].clientAddress}</div>
+            `<div class="invoice" onclick="openInvoice(${invoices[i].invoiceId})">
+                <div class="column-id">${invoices[i].invoiceId}</div>
+                <div class="column-name">${invoices[i].clientName}</div>
+                <div class="column-nui">${invoices[i].clientNUI}</div>
+                <div class="column-add">${invoices[i].clientAddress}</div>
             </div>`
     }
+}
+
+
+function openInvoice(invoiceId) {
+    alert(invoiceId)
 }
 
 renderInvoices()
