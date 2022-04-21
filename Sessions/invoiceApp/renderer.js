7372c69ad511594efc9c4ed3b968279ddc825c00
durimkepuska <durimkepuska@gmail.com>
2022-04-21 20:21:55 +0200

    lift v3

--- Sessions/invoiceApp/renderer.js
+++ Sessions/invoiceApp/renderer.js
@@ -1,1 +1,14 @@
-console.log(invoices)
+function renderInvoices() {
+    for (let i = 0; i < invoices.length; i++) {
+        document.getElementById("invoices").innerHTML =
+            document.getElementById("invoices").innerHTML +
+            `<div class="invoice">
+                <div>${invoices[i].invoiceId}</div>
+                <div>${invoices[i].clientName}</div>
+                <div>${invoices[i].clientNUI}</div>
+                <div>${invoices[i].clientAddress}</div>
+            </div>`
+    }
+}
+
+renderInvoices()
