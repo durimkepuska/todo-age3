ac27bcb07dbfbfed2fcb2e75ecc0e4eafd26c650
durimkepuska <durimkepuska@gmail.com>
2022-05-18 20:33:02 +0200

    bank

--- Sessions/JS classes/class.js
+++ Sessions/JS classes/class.js
@@ -1,28 +1,64 @@
 class Bank {
-    accountNumber;
-    bilance = 0;
-    transaction;
+    #accountNumber;
+    #bilance = 0;
+    #transaction;
+
+    #pin = 8888;
+    #clientAccountNumber = 123456789;
+    #loggedIn = false;
+
+    kyqu(id, pin) {
+        if (id === this.#clientAccountNumber && this.#pin === pin) {
+            this.#loggedIn = true;
+            console.log('success')
+        } else {
+            console.error("Login failed")
+        }
+    }
 
     deposit(value) {
-        if(value < 0){
-            console.error("Vetem depozita pozitive")
-            return
+        if (this.#loggedIn) {
+            if (value < 0) {
+                console.error("Vetem depozita pozitive")
+                return
+            }
+            this.#bilance = this.#bilance + value
+        } else {
+            console.error("Login failed")
         }
-        this.bilance = this.bilance + value
     }
 
-    credit() {
+    credit(value) {
+        if (this.#loggedIn) {
+            if (value > this.#bilance) {
+                console.error("Nuk keni fonde te mjaftueshme");
+                return;
+            }
+            this.#bilance = this.#bilance - value;
+        } else {
+            console.error("Login failed")
+        }
+    }
+
+    transfer(id, value) {
+        if (this.#loggedIn) {
+            this.credit(value);
+        } else {
+            console.error("Login failed")
+        }
 
     }
 
-    transfer() {
-
+    showBilance() {
+        return this.#bilance
     }
 }
 
 const bank = new Bank();
-bank.deposit(-50);
-bank.deposit(-500);
-bank.deposit(50);
-bank.deposit(-50);
-console.log(bank.bilance)
+bank.kyqu(123456789, 8888)
+
+bank.deposit(5000);
+bank.credit(500)
+bank.transfer("56465465465465", 500)
+
+console.log('bilanci i juaj', bank.showBilance())
