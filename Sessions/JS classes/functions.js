d58d3afd0b37b6f3a5a000f3fa789f7aaa4a5dc5
dinagetoar <dinagetoar@gamil.com>
2022-05-23 19:53:06 +0200

    Modified Style on ATM

--- Sessions/JS classes/functions.js
+++ Sessions/JS classes/functions.js
@@ -1,10 +1,24 @@
 let bank;
 
 function login() {
+
     const accountId = document.getElementById('account-number').value;
     const pin = document.getElementById('pin').value;
     bank = new Bank(pin, accountId);
-    updateBilance()
+    updateBilance()    
+    // document.querySelector('.login-box').style.display = 'none';
+    // document.querySelector('.options').style.display = 'flex';
+    let logOn = document.querySelector(".login-box");
+    let logOff = document.querySelector(".options");
+
+    if (document.querySelector(".options").style.display === 'none') {
+        document.querySelector(".login-box").style.display = 'none';
+        document.querySelector(".options").style.display = 'flex';
+    } 
+    else {
+        document.querySelector(".options").style.display = 'none';
+    }
+    
 }
 
 function getMoney() {
@@ -15,6 +29,13 @@ function getMoney() {
 
 function updateBilance() {
     document.getElementById("bilance").innerText = bank.showBilance()
+    document.getElementById("bilance").style = `  color: white;
+    text-align: center;
+    padding: 1px;
+    font-size: 60px;
+    border-bottom: 1px solid #0008f5;
+    margin-bottom: 35px;
+    width: 40%;`
 }
 
 function sendMoney() {
@@ -25,5 +46,17 @@ function sendMoney() {
 
 function logout(){
     bank.logout();
-    document.getElementById("bilance").innerText = ''
+    document.getElementById("bilance").innerText = '';
+    document.getElementById("bilance").style.border ='none';
+    document.querySelector(".options").style.display = 'none';
+    document.querySelector(".login-box").style.display ='block';
+    
+
 }
+
+// let login = document.querySelector('.login-box');
+// let dashboard = document.querySelector('.options');
+// if(){
+//     document.querySelector('.login-box');
+    
+// }
