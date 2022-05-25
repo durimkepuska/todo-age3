17f089f1a8b56b7d77c8ab162840810fb455aadc
durimkepuska <durimkepuska@gmail.com>
2022-05-25 20:17:15 +0200

    gender

--- Sessions/rest call/logic.js
+++ Sessions/rest call/logic.js
@@ -2,8 +2,13 @@ function sendGetRequest() {
     const name = document.getElementById("name").value;
     const response = fetch("https://api.genderize.io?name=" + name);
     response.then(res => res.json()).then(data => {
-        console.log(data)
+        document.getElementById("gender").innerText = data.gender;
+        document.getElementById("percentage").innerText = data.probability;
     });
 
 }
 
+
+//https://api.genderize.io?name=hasan
+//https://api.agify.io/?name=eden
+//https://api.nationalize.io/?name=michael
