71f8ab3cbd697e4e6ff8d6d13288a7c5d6e6b435
durimkepuska <durimkepuska@gmail.com>
2022-05-25 20:43:53 +0200

    gender

--- Sessions/rest call/logic.js
+++ Sessions/rest call/logic.js
@@ -1,13 +1,32 @@
-function sendGetRequest() {
+function checkGender() {
+    document.getElementById("gender").innerText = '';
+    document.getElementById("percentage").innerText = '';
+    document.getElementById("age").innerText = '';
+
     const name = document.getElementById("name").value;
     const response = fetch("https://api.genderize.io?name=" + name);
     response.then(res => res.json()).then(data => {
         document.getElementById("gender").innerText = data.gender;
         document.getElementById("percentage").innerText = data.probability;
+        if (data.gender === 'male') checkAge()
+    });
+}
+
+function checkAge() {
+    const name = document.getElementById("name").value;
+    const response = fetch("https://api.agify.io/?name=" + name);
+    response.then(res => res.json()).then(data => {
+        document.getElementById("age").innerText = data.age;
     });
 
 }
 
+function checkNationality(){
+    //1. get input name
+    //1. get request me emrin qe e kemi shkruar
+    //3. prite rezultatin dhe hyre ne loop ne vektorin country
+    //4. shkruaj ne html secilin country dhe perqindjen me radhe
+}
 
 //https://api.genderize.io?name=hasan
 //https://api.agify.io/?name=eden
