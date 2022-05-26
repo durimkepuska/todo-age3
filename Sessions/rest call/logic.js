842c5fee49e379ef1bae506445617c48e25acb72
durimkepuska <durimkepuska@gmail.com>
2022-05-26 19:39:12 +0200

    fixes

--- Sessions/rest call/logic.js
+++ Sessions/rest call/logic.js
@@ -2,6 +2,7 @@ function checkGender() {
     document.getElementById("gender").innerText = '';
     document.getElementById("percentage").innerText = '';
     document.getElementById("age").innerText = '';
+    document.getElementById("country").innerText = '';
 
     const name = document.getElementById("name").value;
     const response = fetch("https://api.genderize.io?name=" + name);
@@ -17,13 +18,24 @@ function checkAge() {
     const response = fetch("https://api.agify.io/?name=" + name);
     response.then(res => res.json()).then(data => {
         document.getElementById("age").innerText = data.age;
+        checkNationality();
     });
 
 }
 
-function checkNationality(){
-    //1. get input name
-    //1. get request me emrin qe e kemi shkruar
+function checkNationality() {
+    const name = document.getElementById("name").value;
+    const response = fetch("https://api.nationalize.io/?name=" + name);
+    response.then(res => res.json()).then(data => {
+        for (let i = 0; i < data.country.length; i++) {
+            const country = data.country[i];
+
+            document.getElementById("country").innerText =
+                document.getElementById("country").innerText +
+                "\n" + country.country_id + " - " + country.probability;
+
+        }
+    });
     //3. prite rezultatin dhe hyre ne loop ne vektorin country
     //4. shkruaj ne html secilin country dhe perqindjen me radhe
 }
