a87c207492efaea41c917266c4deb68d66a52523
durimkepuska <durimkepuska@gmail.com>
2022-06-15 20:13:01 +0200

    fixes

--- Sessions/express/index.js
+++ Sessions/express/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 8080
 let count = 0;
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/count', (req, res) => {
   count = count + 1;
